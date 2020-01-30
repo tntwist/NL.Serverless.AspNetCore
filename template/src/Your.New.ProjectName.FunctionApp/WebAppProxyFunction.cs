372cc98dd880d218819917a95517e89efa3b5ed1
Nico Lachmuth <nico@nlachmuth.de>
2020-01-30 22:14:02 +0100

    bump to v3.0.3

---
+++ template/src/Your.New.ProjectName.FunctionApp/WebAppProxyFunction.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using NL.Serverless.AspNetCore.AzureFunctionsHost;
+using System.Threading.Tasks;
+
+namespace Your.New.ProjectName.FunctionApp
+{
+    /// <summary>
+    /// Proxy function to delegate incoming http request to web app.
+    /// </summary>
+    public class WebAppProxyFunction : WebAppProxyFunctionBase
+    {
+        public WebAppProxyFunction(IFunctionsRequestHandler requestHandler) : base(requestHandler)
+        {
+        }
+
+        [FunctionName("WebAppProxyFunction")]
+        public async Task<IActionResult> Run(
+           [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", "put", "patch", "delete", "options", Route = "{*any}")] HttpRequest req,
+           ILogger log)
+        {
+            return await ProcessRequestAsync(req, log);
+        }
+    }
+}
